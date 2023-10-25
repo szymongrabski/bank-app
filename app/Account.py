c9571c7f2db0726898ab99cacbe9e5d93ed2b984
szymongrabski <grabskiszymonom@gmail.com>
2023-10-25 17:17:45 +0200

    add transfer history but not working

--- app/Account.py
+++ app/Account.py
@@ -2,15 +2,22 @@ class Account:
     def __init__(self):
         self.saldo = 0
         self.express_transfer_fee = 1
+        self.transfer_history = []
 
     def incoming_transfer(self, amount):
         if amount > 0:
             self.saldo += amount
+            self.transfer_history.append(amount)
 
     def outgoing_transfer(self, amount):
         if 0 < amount <= self.saldo:
             self.saldo -= amount
+            self.transfer_history.append(-amount)
 
     def express_transfer(self, amount):
         if 0 < amount <= self.saldo:
             self.saldo -= (amount + self.express_transfer_fee)
+            self.transfer_history.append((-amount, -self.express_transfer_fee))
+    
+
+    
